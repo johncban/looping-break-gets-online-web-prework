e5510395595f45ea6aa771bcc72d5976ced1c459
johncban <johncban@gmail.com>
2019-04-14 19:08:43 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,12 @@
 
 def levitation_quiz
 	#your code here
+  loop do 
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+    break if answer == "STOP"
+  end
+  puts "Okay, okay, I'll stop"
 end
 
 
